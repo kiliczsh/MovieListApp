c889ff95e5ffa6aa039d1c1bc98b6458efc2aa39
kiliczsh <muhammeddkilicc@gmail.com>
2019-05-04 10:56:53 +0300

    Root changed to 'searches#index'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :watchlists
   resources :searches
+
+  root to: 'searches#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
